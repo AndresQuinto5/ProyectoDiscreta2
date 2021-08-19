1aaa858be83c543a4fa903753d6fc41b7c6a28f5
Andreé Toledo <edgar.andree20@gmail.com>
2021-08-19 16:39:53 -0600

    Update Proyecto2.py

--- Proyecto2.py
+++ Proyecto2.py
@@ -51,28 +51,30 @@ a = int(input("Primero: "))
 b = int(input("Segundo: "))
 print("gcd(", a , "," , b, ") = ", gcd(a, b))
 
+#EJERCICIO 1
+
 # Program to check if a number is prime or not
 
-num = 407
+#num = 407
 
 # To take input from the user
-#num = int(input("Enter a number: "))
+num = int(input("Ingrese un numero: "))
 
 # prime numbers are greater than 1
 if num > 1:
    # check for factors
    for i in range(2,num):
        if (num % i) == 0:
-           print(num,"is not a prime number")
-           print(i,"times",num//i,"is",num)
+           print(num,"no es un numero primo")
+           print(i,"veces",num//i,"es",num)
            break
    else:
-       print(num,"is a prime number")
+       print(num,"es un numero primo")
        
 # if input number is less than
 # or equal to 1, it is not prime
 else:
-   print(num,"is not a prime number")
+   print(num,"no es un numero primo")
 
 
 
